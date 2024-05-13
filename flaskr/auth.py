d4e03256e1d2e5453e4d727d61a9a8a9a4c78bc8
Jenn Cox <jenncox@Jenns-MacBook-Air.local>
2024-05-13 13:17:26 -0400

    finished auth blueprint, created auth templates

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -9,6 +9,7 @@ from flaskr.db import get_db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
+# register view function
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
   if request.method == 'POST':
@@ -38,6 +39,7 @@ def register():
 
   return render_template('auth/register.html')
 
+#login view function
 @bp.route('/login', methods=('GET', 'POST'))
 def login():
   if request.method == 'POST':
@@ -71,4 +73,21 @@ def load_logged_in_user():
   else:
     g.user = get_db().execute(
       'SELECT * FROM user WHERE id = ?', (user_id,)
-    ).fetchone() 
+    ).fetchone()
+
+#logout function
+@bp.route('/logout') 
+def logout():
+  session.clear()
+  return redirect(url_for('index'))
+
+# requiring authentication for other views
+def login_required(view):
+  @functools.wraps(view)
+  def wrapped_view(**kwargs):
+    if g.user is None:
+      return redirect(url_for('auth.login'))
+
+    return view(**kwargs)
+  
+  return wrapped_view
